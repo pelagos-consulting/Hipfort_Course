987f604cd3e1f7b58fcd502c1a8248a9bc121d09
Toby Potter <tobympotter@gmail.com>
2024-01-03 15:11:08 +1100

    Built simple tensoradd programs

---
+++ Fortran_Refresher/tensoradd_simple.f90
@@ -0,0 +1,57 @@
+
+program tensoradd
+
+    ! Add this to make sure that all variables must be declared
+    ! and the compiler performs no type inferencing based on the 
+    ! on the first letter of variable names
+    implicit none
+
+    ! Number of elements in the tensors
+    integer, parameter :: N=16
+
+    ! Upper and lower bounds for testing purposes
+    real :: scratch, upper, lower
+
+    ! Tensor index
+    integer :: i
+
+    ! Define the tensors to use
+    ! Memory will be allocated on the stack
+    real :: A_h(N), B_h(N), C_h(N)
+
+    ! Fill arrays with random numbers using the
+    ! Fortran intrinsic function "random_number"
+    call random_number(A_h)
+    call random_number(B_h)
+
+    ! Run the tensor addition one element at a time
+    do i=1,N
+        C_h(i) = A_h(i) + B_h(i)
+    end do
+
+    ! Could also do it this way
+    C_h(:) = A_h(:) + B_h(:)
+
+    ! Or even this way
+    C_h = A_h + B_h
+
+    ! Check the answer
+    do i=1,N
+        ! The computed solution
+        scratch = A_h(i) + B_h(i)
+
+        ! Get upper and lower bounds on the computed solution
+        ! the spacing function gets the floating point spacing
+        ! from one number to the next
+        upper = scratch + 2.0*spacing(abs(scratch))
+        lower = scratch - 2.0*spacing(abs(scratch))
+
+        ! Check to see if the number is in floating point range of the answer
+        if  ( .not. ((lower <= C_h(i)) .and. (Ch(i) <= upper))) then
+            write(*,*) 'Error, calculated answer at index i = ', i, ' was not in range' 
+        end if
+    
+    end do
+    
+end program vecadd
+
