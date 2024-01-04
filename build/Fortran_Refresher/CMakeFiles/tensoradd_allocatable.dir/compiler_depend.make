b15e19ff1ae076f1b909970318881e689332348b
Toby Potter <tobympotter@gmail.com>
2024-01-04 17:26:56 +1100

    Progress on example fortran programs

--- build/Fortran_Refresher/CMakeFiles/tensoradd_allocatable.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for tensoradd_allocatable.
-# This may be replaced when dependencies are built.
--- build/Memory_Management/memcpy_benchmark/CMakeFiles/memcpy_bench.dir/cmake_clean_Fortran.cmake
+++
@@ -1,6 +0,0 @@
-# Remove fortran modules provided by this target.
-FILE(REMOVE
-  "memcpy_bench_tools.mod"
-  "MEMCPY_BENCH_TOOLS.mod"
-  "CMakeFiles/memcpy_bench.dir/memcpy_bench_tools.mod.stamp"
-  )
